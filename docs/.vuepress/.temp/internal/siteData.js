2ca8b5ca0740ef6abd3981f715f3123ecd82d429
atian <atian@micous.com>
2025-07-28 16:59:26 +0800

    deploy

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"我的博客\",\"description\":\"VuePress博客\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/interview/\",\"lang\":\"zh-CN\",\"title\":\"我的博客\",\"description\":\"VuePress博客\",\"head\":[],\"locales\":{}}")
